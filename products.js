2f83b9b65a19ce3cec265759eeb25a5dab941072
Aadil Meraj <aadilmeraj20.08@gmail.com>
2026-02-13 21:06:50 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,15 @@
+const products = [
+  {
+    id: 1,
+    name: "Italian Marble",
+    category: "Marble",
+    price: 0, 
+    note: "Price on request"
+  },
+  {
+    id: 20,
+    name: "Boring Fitting Article 1",
+    category: "Boring",
+    price: 0
+  }
+];
